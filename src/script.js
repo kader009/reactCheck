30870e5cb2a29474951590d06ce9970aee6040d4
kader009 <kadermollarasel000@gmail.com>
2025-01-29 22:52:50 +0600

    arrayrotation done

--- src/script.js
+++ src/script.js
@@ -1,14 +1,26 @@
 // prime number check
 
-function isPrime(n){
-  if(n < 2) return false;
+function isPrime(n) {
+  if (n < 2) return false;
 
-  for(let i = 2; i <= Math.sqrt(n); i++){
-    if( n % i === 0){
-      return false
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
     }
   }
+
   return true;
 }
 
-console.log(isPrime(7));
+console.log(isPrime(19));
+
+// array rotation
+
+function arrayRotation(arr, arrrotation) {
+  for (let i = 0; i < arrrotation; i++) {
+    arr.unshift(arr.pop());
+  }
+  return arr;
+}
+
+console.log(arrayRotation([1, 2, 3, 4, 5], 2));
