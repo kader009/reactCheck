3cafca6d36b2e14d857ab60ca5a38e730e9a47ae
kader009 <kadermollarasel000@gmail.com>
2025-02-04 12:44:50 +0600

    succefully get data from graphql

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { gql, useQuery } from '@apollo/client';
+
+const getUsersList = gql`
+  query getUsers {
+    getUsers {
+      id
+      name
+      age
+      idMerried
+    }
+  }
+`;
+
 function App() {
+  const { data, error, loading } = useQuery(getUsersList);
+
+  if (loading) return <p>Data loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <div>
       <h1>Users</h1>
+      {data?.getUsers?.map((user) => (
+        <div key={user.id}>
+          <span>{user.name}</span>
+        </div>
+      ))}
     </div>
   );
 }
