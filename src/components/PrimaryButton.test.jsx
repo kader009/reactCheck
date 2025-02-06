521922d6470f7246c12726eb4a36b05b67ec1c63
kader009 <kadermollarasel000@gmail.com>
2025-02-06 12:24:24 +0600

    two test case pass for button

--- src/components/PrimaryButton.test.jsx
+++ src/components/PrimaryButton.test.jsx
@@ -9,4 +9,11 @@ describe('primaryButton', () => {
     const element = screen.getByText(/click me/i);
     expect(element).toBeInTheDocument()
   });
+
+  it('should be trigger function when click', () => {
+    render(<PrimaryButton text='submit' onClick={() =>{}}/>);
+
+    const element = screen.getByText(/submit/i);
+    expect(element).toBeInTheDocument()
+  });
 });
