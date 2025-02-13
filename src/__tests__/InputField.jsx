ebc8b149b8f07a32949bdc24b139e6f623664792
kader009 <kadermollarasel000@gmail.com>
2025-02-13 12:06:37 +0600

    userevent create

--- src/__tests__/InputField.jsx
+++ src/__tests__/InputField.jsx
@@ -1,17 +1,23 @@
-import { render, screen } from '@testing-library/react';
+import { logRoles, render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import InputField from '../components/InputField';
 
 describe('getbyRole', () => {
   it('should be return getbyRole', async () => {
-    render(<InputField />);
+    const { container } = render(<InputField />);
+    logRoles(container);
 
     const element = screen.getByRole('textbox');
     const element2 = screen.getByPlaceholderText('user login');
     const element3 = screen.getByPlaceholderText('password');
     const element4 = screen.getByTitle('mixer');
 
-    const getTheMessage = await screen.findByTestId('message', {}, {timeout: 2000});
+    const getTheMessage = await screen.findByTestId(
+      'message',
+      {},
+      { timeout: 2000 }
+    );
+    screen.logTestingPlaygroundURL()
 
     expect(element).toBeInTheDocument();
     expect(element2).toBeInTheDocument();
