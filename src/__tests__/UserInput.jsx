5f8e20ada65b1a1bc3a18d94ad4d987f18a490db
kader009 <kadermollarasel000@gmail.com>
2025-02-13 12:29:10 +0600

    heading pass query

--- src/__tests__/UserInput.jsx
+++ src/__tests__/UserInput.jsx
@@ -1,12 +1,17 @@
 import {render, screen} from '@testing-library/react'
-import { describe, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import UserEvent from '../components/UserEvent'
 
 describe('userEvent', () =>{
   it('Renders a counter with initial value of 0', () =>{
     render(<UserEvent/>)
 
+    const heading = screen.getByRole('heading', {
+      level: 3
+    })
+
+    expect(heading).toBeInTheDocument()
+
   })
 
-  screen.logTestingPlaygroundURL()
 })
