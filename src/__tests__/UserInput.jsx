2f73ea0546a88cafa51deb59c3755e4828347519
kader009 <kadermollarasel000@gmail.com>
2025-02-13 12:57:15 +0600

    user test case pass

--- src/__tests__/UserInput.jsx
+++ src/__tests__/UserInput.jsx
@@ -1,22 +1,27 @@
-import {render, screen} from '@testing-library/react'
-import { describe, expect, it } from 'vitest'
-import UserEvent from '../components/UserEvent'
-import user from '@testing-library/user-event'
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import UserEvent from '../components/UserEvent';
+import user from '@testing-library/user-event';
 
-describe('userEvent', () =>{
-  it('Renders a counter with initial value of 0', () =>{
-    render(<UserEvent/>)
+describe('userEvent', () => {
+  it('Renders a counter with initial value of 0', () => {
+    render(<UserEvent />);
 
     const heading = screen.getByRole('heading', {
-      level: 3
-    })
+      level: 3,
+    });
 
-    expect(heading).toBeInTheDocument()
-  })
+    expect(heading).toBeInTheDocument();
+  });
 
-  it('should increase count upon on page load', async() =>{
-    user.setup()
-    render(<UserEvent/>)
-  })
-
-})
+  it('should increase count upon on page load', async () => {
+    user.setup();
+    render(<UserEvent />);
+    const increase = screen.getByText('Make her love you more!');
+    await user.click(increase);
+    const heading = screen.getByRole('heading', {
+      level: 3,
+    });
+    expect(heading).toHaveTextContent('Your girlfriend loves you 1 times!')
+  });
+});
