aae42497ba85974fbe2c1613d71b02d5d81ece4a
kader009 <kadermollarasel000@gmail.com>
2025-03-06 19:12:32 +0600

    prime number add

--- src/script.js
+++ src/script.js
@@ -264,3 +264,17 @@ function Descending(number) {
 }
 
 // console.log(Descending(123456789));
+
+// prime number
+
+function isPrimes(num) {
+  if (num < 2) return false;
+
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+
+  return true;
+}
+
+// console.log(isPrimes(19));
